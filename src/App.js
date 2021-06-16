04c82fed6b537032dff0318e3e8085b91f5235e1
TimOn <via-lactea757@yandex.ru>
2021-06-16 18:43:20 +0500

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, withRouter } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import store from './redux/reduxStore';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
+//import DialogsContainer from './components/Dialogs/DialogsContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import Music from './components/Music/Music';
 import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
-import ProfileContainer from './components/Profile/ProfileContainer';
+// import ProfileContainer from './components/Profile/ProfileContainer';
 import Settings from './components/Settings/Settings';
 import UsersContainer from './components/Users/UsersContainer';
 import { initializeApp} from './redux/appReducer';
 import Preloader from './components/Common/Preloader/Preloader';
 import './App.css';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {
 
@@ -36,9 +39,19 @@ class App extends React.Component {
           <Navbar store={this.props.store} />
           <div className='app_wrapper_content'>
             <Route path='/dialogs'
-              render={() => <DialogsContainer />} />
+              render={() => {
+                  return <Suspense fallback={<div>...loading</div>}>
+                    <DialogsContainer />
+                  </Suspense>
+                }
+              } />
             <Route path='/profile/:userId?'          // знак ? в записи :userId? - означает, что данный параметр не обязателен (опционален) --- если его не будет, перейдем на страницу profile
-              render={() => <ProfileContainer store={this.props.store} />} />
+              render={() => {
+                return <Suspense fallback={<div>...loading</div>}>
+                  <ProfileContainer store={this.props.store} />
+                </Suspense>
+                }
+              } />
             <Route path='/users'
               render={() => <UsersContainer />} />
             <Route path='/login'
